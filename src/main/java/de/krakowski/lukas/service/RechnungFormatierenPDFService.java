1e7d8e01b5c6e5fe23bf60e6838e617e96dc56c6
Lukas Krakowski - TGI13 <hss-lukr2806@hssfds.de>
2026-02-06 17:38:12 +0100

    html in PdDokument umgewandelt

--- src/main/java/de/krakowski/lukas/service/RechnungFormatierenPDFService.java
+++ src/main/java/de/krakowski/lukas/service/RechnungFormatierenPDFService.java
@@ -1,9 +1,49 @@
 package de.krakowski.lukas.service;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 
 // Bekomme Alle Daten um PDF Felder auszufüllen gebe fertige PDF an "RechnungBereitstellenController" + Name des Klienten
 
 public class RechnungFormatierenPDFService {
 
+    private void rechnungFormatieren() throws IOException {
+
+    // HTML-Datei aus "resources" laden
+    InputStream htmlRechnungStream = getClass().getResourceAsStream("/rechnungPDF.html");
+
+    if (htmlRechnungStream == null) {
+        throw new IllegalStateException("Resource 'rechnungPDF.html' wurde nicht gefunden");
+    }
+
+    // HTML-Inhalt als String
+    String htmlRechnung = new String(htmlRechnungStream.readAllBytes(), StandardCharsets.UTF_8);
+
+    // String zu byts (html Daten)
+    ByteArrayOutputStream pdfOut = new ByteArrayOutputStream();
+
+    PdfRendererBuilder builder = new PdfRendererBuilder();
+    builder.withHtmlContent(htmlRechnung, null);
+    builder.toStream(pdfOut);
+    builder.run();
+
+    byte[] pdfRechnungsBytes = pdfOut.toByteArray();
+
+    //
+
+    PDDocument documentRechnung = PDDocument.load(new ByteArrayInputStream(pdfRechnungsBytes));
+
     
+    //htmlRechnungStream = HTML 
+    //htmlRechnung = String
+    //pdfRechnungsBytes = Byte
+    //documentRechnung = PDDocument
+
+    }   
 }
