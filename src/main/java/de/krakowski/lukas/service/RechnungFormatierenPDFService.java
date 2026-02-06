92717ec06aebc7c119b2ee2f99c8f5993766b064
Lukas Krakowski - TGI13 <hss-lukr2806@hssfds.de>
2026-02-06 10:43:00 +0100

    angefangen html(pdf)zu erstellen. Gescheitert beim versuch aus der html ein pdf zu machen

--- src/main/java/de/krakowski/lukas/service/RechnungFormatierenPDFService.java
+++ src/main/java/de/krakowski/lukas/service/RechnungFormatierenPDFService.java
@@ -1,9 +1,9 @@
 package de.krakowski.lukas.service;
 
+
 // Bekomme Alle Daten um PDF Felder auszufüllen gebe fertige PDF an "RechnungBereitstellenController" + Name des Klienten
 
 public class RechnungFormatierenPDFService {
 
-
+    
 }
-
