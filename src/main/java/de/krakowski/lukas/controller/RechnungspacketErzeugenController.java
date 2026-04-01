fc64c56fe63b34751bc9846b8255bd95d2251879
Lukas Krakowski - TGI13 <hss-lukr2806@hssfds.de>
2026-04-01 14:55:55 +0200

    änderung an RechnungspacketErzeugenController und RechnungTO. Keine genaueren Infos...

--- src/main/java/de/krakowski/lukas/controller/RechnungspacketErzeugenController.java
+++ src/main/java/de/krakowski/lukas/controller/RechnungspacketErzeugenController.java
@@ -1,9 +1,26 @@
 package de.krakowski.lukas.controller;
 
-
+import de.krakowski.lukas.model.RechnungTO;
+import de.krakowski.lukas.service.RechnungFormatierenPDFService;
+import javafx.scene.image.Image;
 
 public class RechnungspacketErzeugenController {
 
+    private Image pdfImage;
+    private byte[] pdfBytes;
+    private String nameKlient;
+    private RechnungFormatierenPDFService rechnungFormatierenPDFService = new RechnungFormatierenPDFService();
 
+    public RechnungspacketErzeugenController(){
+
+         
+
+        
+    }
+
+    public RechnungTO rechnungsPacketObjektErstellen(){
+
+        return new RechnungTO(null, null, null);
+    }
 
 }
--- src/main/java/de/krakowski/lukas/model/RechnungTO.java
+++ src/main/java/de/krakowski/lukas/model/RechnungTO.java
@@ -1,17 +1,54 @@
 package de.krakowski.lukas.model;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
-
-import de.krakowski.lukas.service.RechnungFormatierenPDFService;
 import javafx.scene.image.Image;
 
 
 public class RechnungTO {
 
+    //-----------Varaiablen-------------
     private Image pdfImage;
-    private PDDocument pdDocument;
+    private byte[] pdfBytes;
+    private String nameKlient;
+    //----------------------------------
 
-    
+    //-----------Konstruktor------------
+    public RechnungTO(Image pdfImage, byte[] pdfBytes, String nameKlient){
+
+        this.pdfImage = pdfImage;
+        this.pdfBytes = pdfBytes;
+        this.nameKlient= nameKlient;
+    }
+    //----------------------------------
 
 
+
+    //---Image---geter&setter-----------
+    public Image getPdfImage() {
+        return pdfImage;
+    }
+
+    public void setPdfImage(Image pdfImage) {
+        this.pdfImage = pdfImage;
+    }
+    //-----------------------------------
+
+    //---Bytes---geter&setter------------
+    public byte[] getPdfBytes() {
+        return pdfBytes;
+    }
+
+    public void setPdfBytes(byte[] pdfBytes) {
+        this.pdfBytes = pdfBytes;
+    }
+    //-----------------------------------
+
+    //---Name---geter&setter-------------
+    public String getNameKlient() {
+        return nameKlient;
+    }
+
+    public void setNameKlient(String nameKlient) {
+        this.nameKlient = nameKlient;
+    }
+    //-----------------------------------
 }
